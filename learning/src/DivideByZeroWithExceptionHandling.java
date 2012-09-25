b239d0da801eed9c0496d9d402a584873b7d8955
A Beethovn <a.beethovn@gmail.com>
2012-09-25 16:40:53 -0700

    deleted a whole bunch of stuff ...

--- learning/src/DivideByZeroWithExceptionHandling.java
+++ learning/src/DivideByZeroWithExceptionHandling.java
@@ -1,21 +1,4 @@
-    // Fig.   . : DivideByZeroWithExceptionHandling.java
-     // Handling ArithmeticExceptions and InputMismatchExceptions.
- /*    import java.util.InputMismatchException;
-     import java.util.Scanner;
-  
-     public class DivideByZeroWithExceptionHandling
-     {
-        // demonstrates throwing an exception when a divide-by-zero occurs
-        public static int quotient( int numerator, int denominator )
-           throws ArithmeticException
-        {
-           return numerator / denominator; // possible division by zero
-        } // end method quotient
-  
-        public static void main( String[] args )
-        {
-           Scanner scanner = new Scanner( System.in ); // scanner for input
-           boolean continueLoop = true; // determines if more input is needed
+/*
   
 ---------------------------------------------------------------------------------
 changes in the branch one
@@ -33,12 +16,6 @@ make some changes to branch 1 (master branch) and then commit and push
 
 
 branch two created 
-
-
-now I am changing this whole thing again.
-must have a conflict
-
-
 
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
               System.out.println(                                           
